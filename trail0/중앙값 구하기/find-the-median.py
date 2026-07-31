098d6428dc69199184dd6c1cca642561b2307f7c
DanielLee <daniel010309@gmail.com>
2026-07-31 16:30:01 +0900

    [Passed] 중앙값 구하기 (51ms, 16MB)

--- trail0/중앙값 구하기/find-the-median.py
+++ trail0/중앙값 구하기/find-the-median.py
@@ -3,17 +3,15 @@ a,b,c = map(int, input().split())
 if (a>b):
     if(c>a):
         print(a)
-    else:
-        if(b>c):
-            print(b)
-        else:
-            print(c)
-
-if (b>a):
-    if (c>b):
+    elif(b>c):
         print(b)
     else:
-        if(a>c):
-            print(a)
-        else:
-            print(c)
+        print(c)
+
+else:
+    if(c>b):
+        print(b)
+    elif(a>c):
+        print(a)
+    else:
+        print(c)
