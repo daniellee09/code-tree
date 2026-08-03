c3f9e36720c5203095c5a09fa5d1d7e89b82faa9
DanielLee <daniel010309@gmail.com>
2026-08-03 09:15:49 +0900

    [Passed] 1부터의 합 (53ms, 16MB)

--- trail0/1부터의 합/sum-from-the-one.py
+++ trail0/1부터의 합/sum-from-the-one.py
@@ -1,11 +1,9 @@
 n = int(input())
 sum = 0
-cnt = 0
 
 for i in range(1, 101):
-    if sum >= n:
-        break
     sum += i
-    cnt = i
+    if sum >= n:
+        print(i)
+        break
 
-print(cnt)
