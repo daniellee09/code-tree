ad24d290b549f839704ff8dd9f9bda4a5cf9ed89
DanielLee <daniel010309@gmail.com>
2026-08-10 11:46:19 +0900

    [Passed] 나누고 빼면서 합하기 (46ms, 16MB)

---
+++ trail2/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -0,0 +1,16 @@
+n,m = map(int,input().split())
+a = list(map(int,input().split()))
+
+def func1(a,m):
+    total = 0
+    while m > 0:
+        total += a[m-1]
+        if m % 2 == 0:
+            m //= 2
+        else:
+            m -= 1
+        
+    return total
+
+print(func1(a,m))
+        
