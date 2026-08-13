20c58d49aa324e6a2b4595d1989008276b4180c2
DanielLee <daniel010309@gmail.com>
2026-08-13 13:52:14 +0900

    [Passed] 재귀함수를 이용한 최소공배수 (50ms, 16MB)

---
+++ trail2/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,21 @@
+def gcd(a,b):
+    while b > 0:
+        a, b = b, a%b
+    return a
+
+def lcm(a,b):
+    return (a*b) // gcd(a,b)
+
+def solution(arr):
+    answer = arr[0]
+    
+    for num in arr[1:]:
+        answer = lcm(answer,num)
+    
+    return answer
+
+
+n = int(input())
+numbers = [int(x) for x in input().split()][:n]
+
+print(solution(numbers))
